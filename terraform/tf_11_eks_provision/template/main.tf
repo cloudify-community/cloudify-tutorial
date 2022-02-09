21cb2ddc22d4364bf0cbafe5eba13ea9f10c592d
warolv <warolv@gmail.com>
2022-02-09 17:04:41 +0200

    Update eks tf files

--- terraform/tf_11_eks_provision/template/main.tf
+++ terraform/tf_11_eks_provision/template/main.tf
@@ -1,25 +1,80 @@
-# Specify the provider and access details
+variable "cluster_name" {
+  default = "eks-training"
+}
+
 provider "aws" {
   region = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-resource "aws_instance" "example_vm" {
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
-  connection {
-    # The default username for our AMI
-    user = var.admin_user
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+data "aws_availability_zones" "available" {
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.0"
+
+  name                 = "k8s-${var.cluster_name}-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24"]
+  public_subnets       = ["172.16.3.0/24", "172.16.4.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
   }
 
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "example-vm"
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
   }
+}
 
-  # Lookup the correct AMI based on the region
-  # we specified
-  ami = lookup(var.aws_amis, var.aws_region)
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.24.0"
+
+  cluster_name    = "${var.cluster_name}"
+  cluster_version = "1.21"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+  write_kubeconfig   = true
+
+  worker_groups = [
+    {
+        name = "ng-medium"
+        instance_type = "t3.medium"
+        asg_desired_capacity = 1
+        tags = [{
+          key                 = "instance-type"
+          value               = "on-demand-medium"
+          propagate_at_launch = true
+        }, {
+          key                 = "os-type"
+          value               = "linux"
+          propagate_at_launch = true
+        }]
+    },
+  ]
 }
